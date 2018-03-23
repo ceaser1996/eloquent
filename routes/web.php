f2c770f644635cb03b523c879fa7f9fe063ee661
leoponking <leoponking@leoponking-Inspiron-3558>
2018-03-23 18:13:24 +0530

    error fixed against xsdsds

--- routes/web.php
+++ routes/web.php
@@ -20,15 +20,62 @@ Route::get('/', function () {
 |--------------------------------------------------------------------------
 |
 */
-Route::get('/find', function()
-{
-//    for all records
-// $post=Post::all();
-// foreach ($post as $posts) {
-//  echo $posts;
+//Route::get('/find', function()
+//{
+////    /*
+////    * for all records in collection
+////    * $posts = collection()
+////     * */
+////    
+// $posts = Post::get()->sortBy('id');
+// 
+////
+//// $posts = Post::orderBy('created_at')->get();
+//// $posts = Post::get()->sortBy('created_at');
+//// 
+////    /*
+////    * get record by primary key
+////    * $post = array()
+////     * */
+////    
+//// $post = Post::where('title', 'sumanta')->exists();
+//// dump($post);
+//// 
+//// $posts = Post::all();
+//// //collections methods
+// 
+// foreach ($posts as $post) {
+//     return $post->get();
 //  
 // }
-    #for one recorf or particular record
-    $post= Post::find(2);
-    return $post->title;
-});
+//});
+//    #for one recorf or particular record
+//    $post= Post::find(2);
+//    return $post->title;
+//});
+/*
+|--------------------------------------------------------------------------
+| Retreiving data using contraints
+|--------------------------------------------------------------------------
+|
+*/
+//Route::get('/findwhere', function (){
+//    $post= Post::orderBy('id','desc')->skip(2)->take(1)->get();
+//    return $post;
+//}
+//    );
+/*
+|--------------------------------------------------------------------------
+| MOre ways to retreive models
+|--------------------------------------------------------------------------
+|
+*/
+Route::get('/findmore',function(){
+//     $post = Post::where('title', 'sumanta')->exists();
+    $post=Post::findOrFail('5');
+    return $post;
+//     dump($post);
+}
+    );
+ 
+    
